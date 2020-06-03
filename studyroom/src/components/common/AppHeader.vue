a6d3864c400215b6675e6e653e9dd6001fd9b303
N L X <894741265@qq.com>
2020-06-03 15:19:37 +0800

    开始写公共全局组件

---
+++ studyroom/src/components/common/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <p class="tittle">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.tittle{
+  height: 40px;
+}
+</style>
