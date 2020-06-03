474e60f90e342d1cda3bf78076a44a7895dab520
N L X <894741265@qq.com>
2020-06-03 10:56:07 +0800

    再次初始化

--- studyroom/src/main.js
+++ studyroom/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './config/index'
+import './assets/css/normalize.scss'
+import './assets/css/common.scss'
+import './assets/css/iconfont.css'
 Vue.config.productionTip = false
 
 new Vue({
--- studyroom/vue.config.js
+++ studyroom/vue.config.js
@@ -1,11 +1,21 @@
 const envNow = process.env.NODE_ENV
 console.log(envNow)
 module.exports = {
-  publicPath: '/app',
-  outputDir: 'dist',
+  publicPath: '/app', // 部署下的目录
+  outputDir: 'dist', // 打包生成文件名
   assetsDir: 'studyroom/',
   indexPath: 'studyroom/index.html',
-  devServer:{
-    port:'8888'
+  devServer: {
+    host: 'localhost',
+    port: '8888'
+    // proxy:{
+    //   '/app':{
+    //     target:'http://localhost:8080',//请求代理地址
+    //     changeOrigin:true,//是否支持跨域
+    //     pathRewrite:{
+    //       '^/app':''
+    //     }
+    //   }
+    // }
   }
 }
